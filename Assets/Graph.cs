d3085374e4ba0a7bf96b5ea48e9e65472e93de7b
NiKuDev <niek_kuijken@hotmail.com>
2019-12-15 17:42:19 +0100

    - expanded loop to include scaling and correct positioning of the cube-collection
    Signed-off-by: NiKuDev <niek_kuijken@hotmail.com>

--- Assets/Graph.cs
+++ Assets/Graph.cs
@@ -8,15 +8,22 @@ public class Graph : MonoBehaviour
 
     void Awake()
     {
-        // instantiating a object/transform/prefab will return a new transform
-        Transform point = Instantiate(this.PointPrefab);
-        point.localPosition = Vector3.right; // shorthand for writing Vector(1, 0, 0)
+        for(int i = 0; i < 10; i++)
+        {
+            Transform point = Instantiate(this.PointPrefab);
 
-        // we can reuse the variable for the second instantiation, as we're not
-        // going to use it anymore for the first cube
-        point = Instantiate(this.PointPrefab);
-        point.localPosition = Vector3.right * 2f; // shorthand for writing Vector(1, 0, 0)
+            // we'd like the range to be x: -1 to x:1 instead of x:0 to x:2
+            // so substract -1 at the end of the formula
 
+            // edit2: to compensate the width of a prefab (0.2), we'll add 0.5f
+            // to the current iteration. this will center it on -1, -0.8, -0.6
+            // opposed to -1.1, -0.9, -0.7 etc.
+            point.localPosition = Vector3.right * ((i + 0.5f) / 5f - 1f);
+
+            // reduce the scale of the prefab to create space between them
+            // we'll use 5 cubes per 1x/y/z
+            point.localScale = Vector3.one / 5f;
+        }
     }
 
     // Start is called before the first frame update
